6e19361f4d945fdb7b4b698dac766de754cccd74
Waldo Lavaut <waldothedeveloper@gmail.com>
2019-07-10 23:33:12 -0400

    Added the env variables and firebase config'

---
+++ src/firebase/firebase_config.js
@@ -0,0 +1,20 @@
+import app from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
+};
+
+// Initialize Firebase
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+  }
+}
+
+export default Firebase;
